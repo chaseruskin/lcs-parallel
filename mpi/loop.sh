ccc36004bc4f54ae0388bb3f008b940eb97301a5
Joel <joelalvarez@ufl.edu>
2024-04-08 22:01:10 -0400

    Adds mpi benchmarking scripts and results

---
+++ mpi/loop.sh
@@ -0,0 +1,16 @@
+INPUT_SIZE=128
+
+
+for i in {1..9};
+do
+    echo "---------  ${$((128 * i))} ---------" >> runtimes.txt
+    export DATA_FILE="$i"
+    for i in $(seq 1 10);
+    do
+        echo "Test " $i
+        python3 ~/al/cli.py --wait --mpi row_wise_v2.c
+        grep 'time taken' build/last.out >> runtimes.txt
+        
+    done
+done
+
