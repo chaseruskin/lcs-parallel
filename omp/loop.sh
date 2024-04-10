3a6322110d4174d808c6c5d700b03c78e4dbf82a
Chase Ruskin <c.ruskin@ufl.edu>
2024-04-09 23:21:01 -0400

    Updates loop for omp

--- omp/loop.sh
+++ omp/loop.sh
@@ -1,12 +1,13 @@
-INPUT_SIZE=32768
-DATE="$(date '+%Y-%m-%d %H:%M:%S')"
-
-echo "--------- $INPUT_SIZE : $DATE ---------" >> runtimes.txt
-
-for i in $(seq 1 10);
+for i in {7..9};
 do
-    echo "Test " $i
-    python3 ~/eel6763/al/cli.py --wait --openmp row_wise_v2.c
-    grep 'total time taken' build/last.out >> runtimes.txt
-    
+    DATE="$(date '+%Y-%m-%d %H:%M:%S')"
+    echo "---------  $((2**($i-1)*128)) : $DATE ---------" >> runtimes.txt
+    export DATA_FILE="$i"
+    for j in $(seq 1 10);
+    do
+        echo "Test " $j
+        python3 ~/eel6763/al/cli.py row_wise_v2.c --openmp --wait
+        grep 'time taken' build/last.out >> runtimes.txt
+        
+    done
 done
