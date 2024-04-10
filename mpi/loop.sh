b96e9ca3e469b238cf66d18de1173c25b0cf8669
Joel <joelalvarez@ufl.edu>
2024-04-09 23:22:51 -0400

    Updates mpi runtimes and loop

--- mpi/loop.sh
+++ mpi/loop.sh
@@ -1,7 +1,7 @@
 INPUT_SIZE=128
 
 
-for i in {1..9};
+for i in {9..9};
 do
     DATE="$(date '+%Y-%m-%d %H:%M:%S')"
     echo "---------  $((2**($i-1)*128)) : $DATE ---------" >> runtimes.txt
@@ -9,7 +9,7 @@ do
     for j in $(seq 1 10);
     do
         echo "Test " $j
-        python3 ~/al/cli.py --wait --mpi row_wise_v2.c
+        python3 ~/al/cli.py --wait --mpi row_wise_v2_space_opt.c
         grep 'time taken' build/last.out >> runtimes.txt
         
     done
