7ab80d1804bcc95d13d129b7344f0ca8735df7ed
Joel <joelalvarez@ufl.edu>
2024-04-28 21:17:39 -0400

    Updates runtime data

--- mpi-opt/loop.sh
+++ mpi-opt/loop.sh
@@ -1,17 +1,19 @@
-INPUT_SIZE=128
+# INPUT_SIZE=128
 
 
-for i in {9..9};
-do
-    DATE="$(date '+%Y-%m-%d %H:%M:%S')"
-    echo "---------  $((2**($i-1)*128)) : $DATE ---------" >> runtimes.txt
-    export DATA_FILE="$i"
+# for i in {9..9};
+# do
+    # DATE="$(date '+%Y-%m-%d %H:%M:%S')"
+    # echo "---------  $((2**($i-1)*128)) : $DATE ---------" >> runtimes.txt
+    # export DATA_FILE="$i"
     for j in $(seq 1 10);
     do
         echo "Test " $j
-        python3 ~/al/cli.py --wait --mpi row_wise_v2_space_opt.c
-        grep 'time taken' build/last.out >> runtimes.txt
+        python3 ~/al/cli.py --wait --mpi --openmp row_wise_opt.c lcs.c
+        echo "Broadcast Times: " >> results/runtimes.txt
+        grep 'matrix mpi broadcast time' build/last.out >> results/runtimes.txt
+        grep 'Execution time' build/last.out >> results/runtimes.txt
         
     done
-done
+# done
 
